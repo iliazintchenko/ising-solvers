8dc0f05116b8c1b7ae03f4ae354a2d88d1142bbb
Ilia Zintchenko <iliazin@gmail.com>
2024-08-18 14:12:30 -0700

    config -> spins

--- ising_solvers/pfa.py
+++ ising_solvers/pfa.py
@@ -16,7 +16,7 @@ def run(
 
     The cost function being optimized is:
 
-    energy = get_energy(couplings, fields)
+    energy = get_energy(spins, couplings, fields)
 
     Parameters:
         couplings (np.ndarray): The couplings matrix for the system, such that couplings[i,j] is the coupling between spin_i and spin_j.
@@ -43,18 +43,18 @@ def run(
 
     rng = np.random.default_rng(seed)
 
-    # start with random config
-    config = 2 * rng.integers(0, 2, n) - 1
+    # start with random spins
+    spins = 2 * rng.integers(0, 2, n) - 1
 
     # energy that we are optimizing
-    energy = get_energy(config, couplings)
+    energy = get_energy(spins, couplings)
 
     # changes in energy if each respective spin is flipped
-    delta_energies = -2 * config * (couplings @ config)
+    delta_energies = -2 * spins * (couplings @ spins)
 
     # track the lowest energy state we have achieved
     energy_min = energy
-    config_min = config.copy()
+    spins_min = spins.copy()
 
     # prepare noise vector to use the Gumbel-Max trick for sampling:
     # https://lips.cs.princeton.edu/the-gumbel-max-trick-for-discrete-distributions/
@@ -73,20 +73,20 @@ def run(
         energy += delta_energies[i]
 
         # update delta_energies
-        delta_delta_energies = (4 * config[i]) * couplings[i] * config
+        delta_delta_energies = (4 * spins[i]) * couplings[i] * spins
         delta_delta_energies[i] = -2 * delta_energies[i]
         delta_energies += delta_delta_energies
 
         # actually flip the spin
-        config[i] *= -1
+        spins[i] *= -1
 
         # track the lowest energy state
         if energy < energy_min - 1e-06:
             energy_min = energy
-            config_min = config.copy()
+            spins_min = spins.copy()
 
     # if we had any fields cut away the last dummy spin
     if fields is not None:
-        config_min = config_min[-1] * config_min[:-1]
+        spins_min = spins_min[-1] * spins_min[:-1]
 
-    return config_min
+    return spins_min
