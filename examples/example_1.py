2d7451dedb608af1ad47d2fd1d27521e8693f749
Ilia Zintchenko <iliazin@gmail.com>
2024-08-22 11:22:45 -0700

    added torch pfa version

--- examples/example_1.py
+++ examples/example_1.py
@@ -1,30 +1,44 @@
 import numpy as np
-from ising_solvers import pfa, utils
+from ising_solvers import pfa, pfa_parallel, utils
 
-couplings = np.array([
-    [ 0.0, -1.0,  0.2,  0.0],
-    [-1.0,  0.0, -1.5,  0.8],
-    [ 0.2, -1.5,  0.0, -1.0],
-    [ 0.0,  0.8, -1.0,  0.0]
-])
+couplings = np.array(
+    [
+        [0.0, -1.1, 0.2, 0.15],
+        [-1.1, 0.0, -1.5, 0.8],
+        [0.2, -1.5, 0.0, -1.0],
+        [0.15, 0.8, -1.0, 0.0],
+    ]
+)
 fields = np.array([0.1, -0.2, 0.3, -0.1])
 offset = 2.5
 
-spins_binary = np.array([0, 0, 0, 1]) 
+spins_binary = np.array([0, 0, 0, 1])
 
 energy_binary = utils.get_energy(spins_binary, couplings, fields, offset)
 
 couplings, fields, offset = utils.binary_to_ising(couplings, fields, offset)
 
-spins_ising = 2 * spins_binary - 1 
+spins_ising = 2 * spins_binary - 1
 
 energy_ising = utils.get_energy(spins_ising, couplings, fields, offset)
 
 np.testing.assert_almost_equal(energy_ising, energy_binary)
 
-spins_min = pfa.run(couplings, fields, beta_max=5.0, num_flips=1000, seed=42)
+beta_max = 3.0
+num_flips = 1000
+num_reps = 100
 
+spins_min = pfa.run(couplings, fields, beta_max=beta_max, num_flips=num_flips, seed=42)
 energy_min = utils.get_energy(spins_min, couplings, fields, offset)
-
 print("Ground state:", spins_min, energy_min)
 
+spins_min = pfa_parallel.run(
+    couplings,
+    fields,
+    beta_max=beta_max,
+    num_flips=num_flips,
+    num_reps=num_reps,
+    seed=42,
+)
+energy_min = utils.get_energy(spins_min, couplings, fields, offset)
+print("Ground state parallel:", spins_min, energy_min)
---
+++ ising_solvers/pfa_parallel.py
@@ -0,0 +1,96 @@
+import numpy as np
+import torch
+from torch.amp import autocast
+
+from torch.backends import cudnn
+
+cudnn.benchmark = True
+cudnn.enabled = True
+
+from .utils import validate_hamiltonian, pack_fields
+
+# @torch.compile
+def run(
+    couplings: np.ndarray,
+    fields: np.ndarray | None = None,
+    beta_max: float = 1.0,
+    num_flips: int = 1000,
+    num_reps: int = 1,
+    seed: int = 42,
+    device: str = "cuda" if torch.cuda.is_available() else "cpu",
+) -> np.ndarray:
+    """
+    Attempts to find the ground state configuration of an Ising spin glass
+    using a GPU-accelerated implementation of the simulated annealing algorithm.
+
+    Parameters:
+        couplings (np.ndarray): The couplings matrix for the system, such that couplings[i,j] is the coupling between spin_i and spin_j.
+        fields (np.ndarray, optional): External field terms. Defaults to None.
+        beta_max (float): The maximum inverse temperature. Defaults to 1.0.
+        num_flips (int): The number of spin flips to perform. Defaults to 1000.
+        seed (int): Seed for the random number generator.
+        device (str): 'cuda' for GPU, 'cpu' for CPU.
+
+    Returns:
+        np.ndarray: The configuration of spins that minimizes the energy.
+    """
+
+    validate_hamiltonian(couplings, fields)
+
+    if beta_max <= 0.0:
+        raise ValueError("beta_max must be positive")
+    if num_flips <= 0:
+        raise ValueError("num_flips must be positive")
+    if num_reps <= 0:
+        raise ValueError("num_reps must be positive")
+
+    couplings = pack_fields(couplings, fields)
+
+    couplings = torch.tensor(couplings, dtype=torch.float32, device=device)
+
+    n = couplings.shape[0]
+
+    torch.manual_seed(seed)
+
+    spins = (2 * torch.randint(0, 2, (n, num_reps), device=device) - 1).float()
+
+    energy_arr = 0.5 * torch.sum(spins * (couplings @ spins), dim=0)
+
+    delta_energies = -2 * spins * (couplings @ spins)
+
+    j = torch.argmin(energy_arr)
+    energy_min = energy_arr[j].clone().detach()
+    spins_min = spins[:, j].clone().detach()
+
+    noise = -torch.log(-torch.log(torch.rand((n, num_reps), device=device)))
+
+    couplings.mul_(4)
+
+    all_reps = torch.arange(num_reps, device=device)
+
+    with autocast(device_type=device):
+
+        for beta in torch.linspace(0.0, beta_max, num_flips, device=device):
+
+            inds = (-beta * delta_energies + noise).argmax(dim=0)
+
+            energy_arr += delta_energies[inds, all_reps]
+
+            delta_energies += spins[inds, all_reps] * couplings[:, inds] * spins
+            delta_energies[inds, all_reps] *= -1
+
+            spins[inds, all_reps] *= -1
+
+            noise = torch.roll(noise, 1, dims=0)
+
+            j = torch.argmin(energy_arr)
+            if energy_arr[j] < energy_min - 1e-06:
+                energy_min = energy_arr[j].clone().detach()
+                spins_min = spins[:, j].clone().detach()
+
+    spins_min = spins_min.cpu().numpy()
+
+    if fields is not None:
+        spins_min = spins_min[-1] * spins_min[:-1]
+
+    return spins_min
